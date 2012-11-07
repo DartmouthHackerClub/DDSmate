86fededa99bc50219021a603d25f2e43bb857148
www-data <www-data@localhost>
2012-11-07 18:07:31 -0500

    Changed lots

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -38,8 +38,9 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-  config.action_controller.relative_url_root = '/ddsmate'
+  #config.action_controller.relative_url_root = '/ddsmate'
   config.action_controller.session_store = :active_record_store
+  #config.serve_static_assets = true
 end
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
@@ -47,3 +48,4 @@ require 'casclient/frameworks/rails/filter'
 CASClient::Frameworks::Rails::Filter.configure(
   :cas_base_url => "https://login.dartmouth.edu/cas"
 )
+
